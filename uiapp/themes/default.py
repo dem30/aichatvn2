e5bded4967b360ca46a59a239f866e97277282fb
dem30 <thaiquangvinh@outlook.com>
2025-09-11 15:50:13 +0700

    Add files via upload

---
+++ uiapp/themes/default.py
@@ -0,0 +1,12 @@
+THEME = {
+    "text_h1": "text-2xl font-bold",  # H1 lớn hơn
+    "text_h2": "text-2xl font-semibold",  # H2 nhỏ hơn, bớt độ đậm
+
+    "primary": "text-blue-600",
+    "secondary": "text-gray-100",
+    "text": "text-gray-800",
+
+    "background": "bg-gray-50",
+    "danger": "bg-red-500 text-white",
+    "card": "bg-white shadow rounded-lg"
+}
